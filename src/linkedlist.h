3e204666ca779e3143c67063013792f2a8545007
Joel Ostos <joelostoscastro@gmail.com>
2023-11-30 01:01:58 -0500

    Creación carpeta tests y nuevos metodos para listas y arreglos

--- src/linkedlist.h
+++ src/linkedlist.h
@@ -1,6 +1,8 @@
 #ifndef _LINKEDLIST_H_
 #define _LINKEDLIST_H_
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
 #define linkedlist(type)						\
   typedef struct LinkedList_##type LinkedList_##type;			\
@@ -31,7 +33,7 @@
       .size = 0,							\
     };									\
   }									\
-  void push_back_##type(LinkedList_##type* list, type data)		\
+  void push_back_list_##type(LinkedList_##type* list, type data)	\
   {									\
     Node_##type* node = create_node_##type(data);			\
     if (!list->head) {							\
@@ -45,5 +47,146 @@
     list->head = node;							\
     list->size++;							\
   }									\
-  
+  void delete_node_list_##type(LinkedList_##type* list, size_t index)	\
+  {									\
+    size_t cont = 0;							\
+    if (index != 0 && index <= list->size/2) {				\
+      Node_##type* current = list->tail;				\
+      while (current){							\
+	if (cont == index){						\
+	  current->prev->next = current->next;				\
+	  current->next->prev = current->prev;				\
+	  free(current);						\
+	  return;							\
+	}								\
+	cont++;								\
+	current = current->next;					\
+      }									\
+      return;								\
+    } else if (index != list->size-1 && index > list->size/2) {	\
+      Node_##type* current = list->head;				\
+      cont = (list->size-1)-index;					\
+      while (current){							\
+	if (cont == index){						\
+	  current->prev->next = current->next;				\
+	  current->next->prev = current->prev;				\
+	  free(current);						\
+	  return;							\
+	}								\
+	cont++;								\
+	current = current->prev;					\
+      }									\
+      return;								\
+    } else if (index == 0){						\
+      Node_##type* tmp = list->tail->next;				\
+      free(list->tail);							\
+      list->tail = tmp;							\
+    }									\
+    Node_##type* tmp = list->head->prev;				\
+    free(list->tail);							\
+    list->head = tmp;							\
+  }									\
+  void deinit_list_##type(LinkedList_##type* list) {			\
+    Node_##type* current = list->tail;					\
+    while (current) {							\
+      Node_##type* tmp = current->next;					\
+      free(current);							\
+      current = tmp;							\
+    }									\
+  }									\
+
+#define arraylist(type)							\
+  typedef struct ArrayList_##type ArrayList_##type;			\
+  struct ArrayList_##type {						\
+    type* array;							\
+    size_t capacity;							\
+    size_t occupied;							\
+    bool state;								\
+  };									\
+  ArrayList_##type init_array_##type(size_t initial_size)		\
+  {									\
+    type* arr = (type*) malloc(sizeof(type)*initial_size+1);		\
+    return (ArrayList_##type) {						\
+      .array = arr,							\
+      .capacity = initial_size,						\
+      .occupied = 0,							\
+      .state = true,							\
+    };									\
+  }									\
+  ArrayList_##type set_array_##type(type* arr, size_t initial_size)	\
+  {									\
+    return (ArrayList_##type) {						\
+      .array = arr,							\
+      .capacity = initial_size,						\
+      .occupied = 0,							\
+      .state = false,							\
+    };									\
+  }									\
+  void push_back_array_##type(ArrayList_##type* array, type data)	\
+  {									\
+    if (array->state){							\
+      if (array->occupied < array->capacity) {				\
+	array->array[array->occupied] = data;				\
+	array->occupied += 1;						\
+	return;								\
+      }									\
+      array->array = realloc(array->array,(array->capacity + 1) * sizeof(type)); \
+      array->array[array->occupied] = data;				\
+      array->occupied += 1;						\
+      array->capacity += 1;						\
+      return;								\
+    }									\
+    if (array->occupied < array->capacity) {				\
+      array->array[array->occupied] = data;				\
+      array->occupied += 1;						\
+      return;								\
+    }									\
+    type* new = (type*) malloc(sizeof(type) * (array->capacity+1));	\
+    array->array = memcpy(new, array->array, sizeof(type) * array->capacity); \
+    array->array[array->occupied] = data;				\
+    array->occupied += 1;						\
+    array->capacity += 1;						\
+    array->state = true;						\
+    return;								\
+  }									\
+  void insert_array_##type(ArrayList_##type* array, type data, size_t index) \
+  {									\
+    if (array->occupied < array->capacity && index <= array->occupied) { \
+      type* new = (type*) malloc(sizeof(type) * array->capacity);	\
+      memcpy(new, array->array, sizeof(type) * index);			\
+      memcpy(new+index, array->array+index-1, sizeof(type) * (array->capacity-index)+1); \
+      new[index] = data;						\
+      free(array->array);						\
+      array->array = NULL;						\
+      array->array = new;						\
+      array->occupied += 1;						\
+      return;								\
+    }else if (array->occupied == array->capacity && index <= array->occupied){ \
+      type* new = (type*) malloc(sizeof(type) * array->capacity+1);	\
+      memcpy(new, array->array, sizeof(type) * index);			\
+      memcpy(new+index, array->array+index-1, sizeof(type) * (array->capacity-index)+1); \
+      new[index] = data;						\
+      free(array->array);						\
+      array->array = NULL;						\
+      array->array = new;						\
+      array->occupied += 1;						\
+      array->capacity += 1;						\
+      return;								\
+    }									\
+  }									\
+  void delete_array_##type(ArrayList_##type* array, size_t index)	\
+  {									\
+    if (array->occupied <= array->capacity && index <= array->occupied) { \
+      memcpy(array->array+index, array->array+index+1, sizeof(type) * (array->capacity-index)); \
+      array->occupied -= 1;						\
+      return;								\
+    }									\
+  }									\
+  void deinit_array(ArrayList_##type* array)				\
+  {									\
+    free(array->array);							\
+    array->array = NULL;						\
+  }									\
+
 #endif
+// TODO on line 105: Design Decision, Bitacora TODO (1)
---
+++ src/test/linked_list_prueba.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include "../linkedlist.h"
+typedef struct Persona Persona;
+
+
+struct Persona {
+  char nombre[300];
+  int edad;
+  int altura;
+};
+
+linkedlist(int)
+linkedlist(char)
+linkedlist(Persona)
+arraylist(int)
+
+Persona crear_persona(int data)
+{
+  return (Persona){
+    "Jose Eustacio", data, data
+  };
+}
+
+void print_int(LinkedList_int* list)
+{
+  Node_int* current = list->tail;
+  while(current) {
+    printf("%d ", current->data);
+    current = current->next;
+  }
+}
+
+void print_persona(LinkedList_Persona* list)
+{
+  Node_Persona* current = list->tail;
+  while (current) {
+    printf("%s \t %d \t %d\n", current->data.nombre, current->data.altura, current->data.edad);
+    current = current->next;
+  }
+}
+
+void print_char(LinkedList_char* list) {
+  Node_char* current = list->tail;
+  while (current) {
+    printf("%c ", current->data);
+    current = current->next;
+  }
+}
+
+int main(void)
+{
+  // ----------- Listas --------------------
+  LinkedList_int lista = init_list_int();
+  LinkedList_char lista_char = init_list_char();
+  LinkedList_Persona lista_persona = init_list_Persona();
+  for (int i = 0; i < 20; i++) {
+    push_back_list_int(&lista, i);
+    push_back_list_char(&lista_char, 'i');
+    push_back_list_Persona(&lista_persona, crear_persona(i));
+  }
+  print_int(&lista);
+  delete_node_list_int(&lista, 10);
+
+  printf("\n------ Lista luego de eliminar ------\n");
+  print_int(&lista);
+  printf("\nDealocando\n");
+  deinit_list_int(&lista);
+  deinit_list_char(&lista_char);
+  deinit_list_Persona(&lista_persona);
+
+  printf("\n------ Arreglos ------\n");
+  // ----------- Arreglos --------------------
+  int array[20];
+  ArrayList_int arreglo = set_array_int(array, 3);
+  ArrayList_int arreglo2 = init_array_int(3);
+  insert_array_int(&arreglo2,103,0);
+  insert_array_int(&arreglo2,104,1);
+  insert_array_int(&arreglo2,105,2);
+  insert_array_int(&arreglo2,107,3);
+  push_back_array_int(&arreglo2, 2);
+  push_back_array_int(&arreglo2, 3);
+  push_back_array_int(&arreglo2, 5);
+  printf("%ld\n", arreglo.capacity);
+  for (size_t i = 0; i < arreglo2.capacity; i++) {
+    printf("%d ", arreglo2.array[i]);
+  }
+  delete_array_int(&arreglo2, 5);
+  delete_array_int(&arreglo2, 0);
+  delete_array_int(&arreglo2, 0);
+  delete_array_int(&arreglo2, 0);
+  delete_array_int(&arreglo2, 0);
+  delete_array_int(&arreglo2, 0);
+  delete_array_int(&arreglo2, 0);
+  delete_array_int(&arreglo2, 0);
+  printf("\n%ld\n", arreglo.capacity);
+
+  for (size_t i = 0; i < arreglo2.capacity; i++) {
+    printf("%d ", arreglo2.array[i]);
+  }
+  deinit_array(&arreglo2);
+  if (arreglo2.array){
+    for (size_t i = 0; i < arreglo2.capacity; i++) {
+      printf("%d ", arreglo2.array[i]);
+    }
+  }
+}
